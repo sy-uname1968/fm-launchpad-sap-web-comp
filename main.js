58c0e4a7d13d6a03ecf2a9abd879ef091afecf2a
sy-uname1968 <67422892+sy-uname1968@users.noreply.github.com>
2022-08-10 08:48:10 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,16 @@
+import "@ui5/webcomponents/dist/TabContainer";
+import "@ui5/webcomponents/dist/Tab";
+import "@ui5/webcomponents/dist/TabSeparator";
+import "@ui5/webcomponents/dist/Avatar";
+import "@ui5/webcomponents-icons/dist/customer"
+import "@ui5/webcomponents-icons/dist/hello-world";
+import "@ui5/webcomponents-icons/dist/collaborate";
+import "@ui5/webcomponents-fiori/dist/ShellBar";
+import "@ui5/webcomponents/dist/Card";
+import "@ui5/webcomponents/dist/CardHeader";
+import "@ui5/webcomponents/dist/Title";
+import "@ui5/webcomponents/dist/Link";
+import { setTheme } from "@ui5/webcomponents-base/dist/config/Theme.js";
+
+
+setTheme("sap_belize_hcb");
